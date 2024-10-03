b05cbbab46e22196572131374c61c723cfe0f88a
Anisah <anisah@terasdigital.com>
2024-10-03 23:34:16 +0800

    Set up backend environment

---
+++ timesheet-backend/timesheet-backend/src/test/java/com/example/timesheet_backend/TimesheetBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.timesheet_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimesheetBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

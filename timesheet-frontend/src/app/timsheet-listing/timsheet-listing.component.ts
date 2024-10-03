76fd32812384ff0688279d05b15cfc3b8596d0d4
nurulanisahasri <85614688+nurulanisahasri@users.noreply.github.com>
2024-10-03 07:21:46 +0000

    Create timesheet-listing folder

---
+++ timesheet-frontend/src/app/timsheet-listing/timsheet-listing.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-timsheet-listing',
+  standalone: true,
+  imports: [],
+  templateUrl: './timsheet-listing.component.html',
+  styleUrl: './timsheet-listing.component.css'
+})
+export class TimsheetListingComponent {
+
+}
